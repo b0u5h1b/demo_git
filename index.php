e283e5fc2c22aefe4be9a271e2c1e580c758b4d2
th3ghos7 <boushib.e@gmail.com>
2021-01-30 23:53:56 +0100

    adjust the index.php skelton

--- index.php
+++ index.php
@@ -1,35 +1,40 @@
-<html>
-  <head>
-    <title>Class and Object Methods!</title>
-  </head>
-  <body>
-      <!-- This is a comment -->
-      <!-- This is a 2nd comment -->
-    <p>
-      <?php
-        class Person {
-          public $isAlive = true;
-          
-          function __construct($name) {
-              $this->name = $name;
-          }
-          
-          public function dance() {
-            return "I'm dancing!";
-          }
-        }
-        
-        $me = new Person("Shane");
-        if (is_a($me, "Person")) {
-          echo "I'm a person, ";
-        }
-        if (property_exists($me, "name")) {
-          echo "I have a name, ";
-        }
-        if (method_exists($me, "dance")) {
-          echo "and I know how to dance!";
-        }
-      ?>
-    </p>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP OOP</title>
+</head>
+
+<body>
+  <?php
+  class Person
+  {
+    public $isAlive = true;
+
+    function __construct($name)
+    {
+      $this->name = $name;
+    }
+
+    public function dance()
+    {
+      return "I'm dancing!";
+    }
+  }
+
+  $me = new Person("Shane");
+  if (is_a($me, "Person")) {
+    echo "I'm a person, ";
+  }
+  if (property_exists($me, "name")) {
+    echo "I have a name, ";
+  }
+  if (method_exists($me, "dance")) {
+    echo "and I know how to dance!";
+  }
+  ?>
+</body>
+
 </html>
